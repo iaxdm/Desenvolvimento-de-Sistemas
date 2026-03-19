c15be333e5f9a60d4d63bef51e0e28683e9a5ec2
Igor De Azevedo Xavier Damieri De Morães <pet0160427@aluno.firjansenaisesi.com.br>
2026-03-18 21:30:41 -0300

    atividade2

--- Atividade2/atividade2.js
+++ Atividade2/atividade2.js
@@ -2,20 +2,23 @@ import express from "express"
 
 const porta = 3000
 const api = express()
+
+//Uma variável de lista inicialmente vazia
+
 const lista = [
     {}
 ]
 
 // Uma rota GET para retornar todos os produtos e pode ter filtragem por nome ou categoria
 
-api.get("/produtos", (req, res) => {  //rota get
-    let resposta = lista //a variável resposta recebe os elementos da lista
-    const filtros = req.querry // a constante filtros recebe um requerimento do tipo querry
+api.get("/produtos", (req, res) => {
+    let resposta = lista
+    const filtros = req.params
 
-    if (filtros.nome){  //se estiver filtrando por nome ou seja req.querry.nome
-        resposta = resposta.filter((produto) => { // resposta recebe respota com filtro e salva como como a variável produto
-            return produto.nome.toLowerCase().includes(filtros.nome.toLowerCase()) //retorna o nome do produto em caixa baixa 
-            // produto.nome.toLowerCase() == 
+    if (filtros.nome){
+        resposta = resposta.filter((produto) => {
+            return produto.nome.toLowerCase().includes(filtros.nome.toLowerCase())
+        
         })
     }
 
