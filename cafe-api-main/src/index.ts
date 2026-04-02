1d7f67e34af38f6a594ec2f82d626ac57d9e999e
Igor De Azevedo Xavier Damieri De Morães <pet0160427@aluno.firjansenaisesi.com.br>
2026-04-01 21:39:21 -0300

    cafe-api-main

---
+++ cafe-api-main/src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import type { Application } from "express";
+import { pool } from "./db.js";
+import { produtoRoutes } from "./routes/produtoRoutes.js";
+
+const app: Application = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use("/api/produtos", produtoRoutes);
+
+app.get("/api", async (req, res) => {
+  try {
+    const { rows } = await pool.query("SELECT NOW()");
+    res.json({ message: "Node 25 + TS rodando!", dbTime: rows[0] });
+  } catch (err) {
+    res.status(500).json({ error: "Erro ao conectar no banco" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Servidor rodando em http://localhost:${port}`);
+});
